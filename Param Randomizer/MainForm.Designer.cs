9c7874617ca1b8419a64e108c30c41f9e2e28893
LingSan_CanMeng <yxp20050828@163.com>
2024-10-16 07:11:09 +0800

    添加项目文件。

---
+++ Param Randomizer/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Param_Randomizer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            talk = new CheckBox();
+            confirm = new Button();
+            browse = new Button();
+            TextBox = new TextBox();
+            openFileDialog1 = new OpenFileDialog();
+            seed = new NumericUpDown();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)seed).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(talk);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(307, 368);
+            panel1.TabIndex = 0;
+            // 
+            // talk
+            // 
+            talk.Anchor = AnchorStyles.Top;
+            talk.AutoSize = true;
+            talk.Checked = true;
+            talk.CheckState = CheckState.Checked;
+            talk.Location = new Point(104, 3);
+            talk.Name = "talk";
+            talk.Size = new Size(92, 21);
+            talk.TabIndex = 1;
+            talk.Text = "Talk Param";
+            talk.UseVisualStyleBackColor = true;
+            // 
+            // confirm
+            // 
+            confirm.Location = new Point(244, 415);
+            confirm.Name = "confirm";
+            confirm.Size = new Size(75, 23);
+            confirm.TabIndex = 1;
+            confirm.Text = "Confirm";
+            confirm.UseVisualStyleBackColor = true;
+            confirm.Click += confirm_Click;
+            // 
+            // browse
+            // 
+            browse.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            browse.Location = new Point(244, 386);
+            browse.Name = "browse";
+            browse.Size = new Size(75, 23);
+            browse.TabIndex = 2;
+            browse.Text = "Browse";
+            browse.UseVisualStyleBackColor = true;
+            browse.Click += browse_Click;
+            // 
+            // TextBox
+            // 
+            TextBox.Location = new Point(12, 386);
+            TextBox.Name = "TextBox";
+            TextBox.ReadOnly = true;
+            TextBox.Size = new Size(226, 23);
+            TextBox.TabIndex = 2;
+            TextBox.Text = " Waiting for regulation";
+            TextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.Filter = "Regulation File|regulation.bin|All Files|*.*";
+            openFileDialog1.Title = "Open \"regulation.bin\"";
+            // 
+            // seed
+            // 
+            seed.Location = new Point(12, 416);
+            seed.Maximum = new decimal(new int[] { 999999999, 0, 0, 0 });
+            seed.Minimum = new decimal(new int[] { 1, 0, 0, int.MinValue });
+            seed.Name = "seed";
+            seed.Size = new Size(226, 23);
+            seed.TabIndex = 3;
+            seed.TextAlign = HorizontalAlignment.Center;
+            seed.Value = new decimal(new int[] { 1, 0, 0, int.MinValue });
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(331, 450);
+            Controls.Add(seed);
+            Controls.Add(TextBox);
+            Controls.Add(browse);
+            Controls.Add(confirm);
+            Controls.Add(panel1);
+            Name = "MainForm";
+            Text = "Param Randomizer";
+            Load += MainForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)seed).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private CheckBox talk;
+        private Button confirm;
+        private Button browse;
+        private TextBox TextBox;
+        private OpenFileDialog openFileDialog1;
+        private NumericUpDown seed;
+    }
+}
---
+++ Param Randomizer/MainForm.cs
@@ -0,0 +1,154 @@
+﻿using SoulsFormats;
+
+namespace Param_Randomizer;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void confirm_Click(object sender, EventArgs e)
+    {
+        if (openFileDialog1.FileName == "")
+        {
+            MessageBox.Show("No file Selected!", "Error", MessageBoxButtons.OK);
+            return;
+        }
+        if (talk.Checked == false)
+        {
+            MessageBox.Show("No Param Selected!", "Error", MessageBoxButtons.OK);
+            return;
+        }
+
+        if (talk.Checked == true)
+        {
+            int rngSeed = (int)seed.Value;
+            if (rngSeed == -1)
+            {
+                //if no seed was provided, generate a new one
+                Random newSeed = new Random();
+                seed.Value = newSeed.Next(1, 999999999);
+                rngSeed = (int)seed.Value;
+            }
+            Random rng = new(rngSeed);
+            randtalk(rng);
+        }
+
+        GC.Collect(); //free memory
+
+        UpdateConsole("Finished!");
+        System.Media.SystemSounds.Exclamation.Play(); //make noise
+        MessageBox.Show("All done!", "Randomization Finished", MessageBoxButtons.OK);
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void browse_Click(object sender, EventArgs e)
+    {
+        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+            string? directory = Path.GetDirectoryName(openFileDialog1.FileName);
+            if (directory != null && File.Exists(directory + "\\eldenring.exe"))
+            {
+                //user is loading the regulation next to eldenring.exe, yell at them
+
+                DialogResult result = MessageBox.Show(
+                    "Warning: Modifying the Regulation.bin directly used by Elden Ring is ill-advised, as it's possible you may be banned."
+                    + " \nIt's highly recommended you instead use Mod Engine 2 and modify a copy of Regulation.bin."
+                    + " \n\nAre you sure you want to load the Regulation.bin you selected?"
+                    , "Confirm Regulation.bin Selection", MessageBoxButtons.OKCancel);
+                if (result != DialogResult.OK)
+                {
+                    openFileDialog1.FileName = "";
+                    return;
+                }
+            }
+
+            UpdateConsole("Selected Regulation.bin");
+        }
+    }
+
+    public void UpdateConsole(string text)
+    {
+        TextBox.Text = text;
+        Application.DoEvents();
+    }
+
+    private void randtalk(Random rng)
+    {
+        Dictionary<string, PARAM> paramList = new();
+        string regulationPath = openFileDialog1.FileName;
+
+        UpdateConsole("Decrypting Regulation");
+
+        BND4 paramBND = SFUtil.DecryptERRegulation(regulationPath);
+
+        UpdateConsole("Loading ParamDefs");
+
+        var paramdefs = new List<PARAMDEF>();
+        var paramdef = PARAMDEF.XmlDeserialize($@"{Directory.GetCurrentDirectory()}\Paramdex\TalkParam.xml");
+        paramdefs.Add(paramdef);
+
+        UpdateConsole("Handling Params");
+
+        foreach (BinderFile file in paramBND.Files)
+        {
+            string name = Path.GetFileNameWithoutExtension(file.Name);
+            var param = PARAM.Read(file.Bytes);
+
+            if (param.ApplyParamdefCarefully(paramdefs))
+                paramList[name] = param;
+        }
+
+        UpdateConsole("Modifying Params");
+
+        PARAM talkParam = paramList["TalkParam"];
+
+
+        List<int> goodIDs = new();
+        for (int i = 0; i < talkParam.Rows.Count; i++)
+        {
+            PARAM.Row row = talkParam.Rows[i];
+
+            if (row.ID is 100 or 200)
+            {
+                continue;
+            }
+
+            goodIDs.Add((int)row["msgId"].Value);
+            goodIDs.Add((int)row["msgId_female"].Value);
+
+            row["msgId"].Value = -1;
+            row["msgId_female"].Value = -1;
+        }
+
+        for (int i = 0; i < talkParam.Rows.Count; i++)
+        {
+            PARAM.Row row = talkParam.Rows[i];
+
+            if (row.ID is 100 or 200)
+            {
+                continue;
+            }
+
+            row["msgId"].Value = goodIDs[rng.Next(0, goodIDs.Count)];
+            row["msgId_female"].Value = goodIDs[rng.Next(0, goodIDs.Count)];
+        }
+
+        UpdateConsole("Exporting Params");
+
+        foreach (BinderFile file in paramBND.Files)
+        {
+            string name = Path.GetFileNameWithoutExtension(file.Name);
+            if (paramList.ContainsKey(name))
+                file.Bytes = paramList[name].Write();
+        }
+
+        SFUtil.EncryptERRegulation(regulationPath, paramBND);
+    }
+}
